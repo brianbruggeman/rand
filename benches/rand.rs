7ff48ce46ce68d9218df043ef1e77b0f464a8ff1
Brian Bruggeman <1350114+brianbruggeman@users.noreply.github.com>
2024-08-28 08:07:48 -0500

    style: fix formatting

--- benches/rand.rs
+++ benches/rand.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId, Bencher};
+use criterion::{black_box, criterion_group, criterion_main, Bencher, BenchmarkId, Criterion};
 use std::time::Instant;
 
 // Function to benchmark
@@ -23,22 +23,27 @@ fn criterion_benchmark(c: &mut Criterion) {
     // Add a custom check for performance regression
     let mut median_ave: f64 = 0.0;
     let mut median_count = 0;
-    group.bench_with_input(BenchmarkId::new("check_regression", "rand"), &expected_median_time_ns, |b, &threshold| {
-        b.iter_custom(|iters| {
-            let mut rng = black_box(rand::Rng::default());
-            let start = Instant::now();
-            for _ in 0..iters {
-                let x = rng.rand();
-                black_box(x);
-            }
-            let elapsed = start.elapsed();
-            let median_time_ns = elapsed.as_nanos() / iters as u128;
-            median_ave = (median_ave as f64 * median_count  as f64 + median_time_ns as f64) / (median_count as f64 + 1.0) as f64;
-            median_count += 1;
+    group.bench_with_input(
+        BenchmarkId::new("check_regression", "rand"),
+        &expected_median_time_ns,
+        |b, &threshold| {
+            b.iter_custom(|iters| {
+                let mut rng = black_box(rand::Rng::default());
+                let start = Instant::now();
+                for _ in 0..iters {
+                    let x = rng.rand();
+                    black_box(x);
+                }
+                let elapsed = start.elapsed();
+                let median_time_ns = elapsed.as_nanos() / iters as u128;
+                median_ave = (median_ave as f64 * median_count as f64 + median_time_ns as f64)
+                    / (median_count as f64 + 1.0) as f64;
+                median_count += 1;
 
-            elapsed
-        });
-    });
+                elapsed
+            });
+        },
+    );
 
     println!("\nMedian {median_ave:.2} ns over {median_count} items.  Threshold set to {expected_median_time_ns} ns");
 
